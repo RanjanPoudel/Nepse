256c91119d4d1f49fd534a3784e6d2fdc14c7394
AutomatePro <wondervba@gmail.com>
2022-04-15 23:02:40 +1000

    First AutoExcelPy

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import pandas as pd
+pd.options.mode.chained_assignment = None # default='warn'
+import pyodbc
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class clsDBProcs:
+
+    def __init__(self, ServerName, DatabaseName):
+        self.ServerName = ServerName,
+        self.DatabaseName = DatabaseName
+
+    def getMyConnectionObj(self, ServerName, DatabaseName):
+
+        try:
+            # connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + ServerName +';DATABASE=' + DatabaseName + ';Trusted_Connection=yes;')
+
+            myConnection = pyodbc.connect("Driver={SQL Server Native Client 11.0}"
+                                        + ";SERVER=" + ServerName
+                                        + ";DATABASE=" + DatabaseName
+                                        + ";Trusted_Connection=yes")
+
+            return myConnection
+
+        except TimeoutError as e:
+            print("couldn't connect for long time")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def getDataFrame_SQL(self, myConnection, MySQLStr):
+
+        try:
+                # Run SQL
+            sql_query = pd.read_sql(MySQLStr, myConnection)
+                # Convert SQL to DataFrame
+            print("now reading data into data frame...")
+            df = pd.DataFrame(sql_query)
+            # print(df.head())
+                # Execute the query
+            return df
+
+        except TimeoutError as e:
+            print("couldn't connect for long time")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+myDBObj  = clsDBProcs('LAPTOP-BSODD31S\SMJ_DEV', 'Investment_DB')
+myConnectionObj = myDBObj.getMyConnectionObj('LAPTOP-BSODD31S\SMJ_DEV', 'Investment_DB')
+# print(engine.table_names())
+sql_query = 'SELECT * FROM [dbo].[StockManagement]'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+myDf = myDBObj.getDataFrame_SQL(myConnectionObj, 'SELECT * FROM [dbo].[TickerPriceDaily]')
+print(myDf.head())
